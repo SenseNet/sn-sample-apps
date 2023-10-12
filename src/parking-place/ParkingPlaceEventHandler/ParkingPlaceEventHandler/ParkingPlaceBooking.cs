abf4cd55b4bf1ef3f84093586c929a2b40f81ff9
kavics <gyeby5@gmail.com>
2023-10-12 16:59:39 +0200

    Add webhook handler service and a webhook subscription. (#6)
    * Add webhook handler service and a webhook subscription.
    
    * EventHandler project refactor.
    
    * Send email notification when a day is full.
    
    ---------
    
    Co-authored-by: tusmester <tusmester@gmail.com>

---
+++ src/parking-place/ParkingPlaceEventHandler/ParkingPlaceEventHandler/ParkingPlaceBooking.cs
@@ -0,0 +1,13 @@
+﻿using SenseNet.Client;
+
+namespace ParkingPlaceEventHandler;
+
+public class ParkingPlaceBooking : Content
+{
+    public ParkingPlaceBooking(IRestCaller restCaller, ILogger<Content> logger) : base(restCaller, logger) { }
+
+    public DateTime ParkingPlaceBookingStart { get; set; }
+    public DateTime ParkingPlaceBookingEnd { get; set; }
+    public Content ParkingPlaceUser { get; set; }
+    public Content ParkingPlace { get; set; }
+}
---
+++ src/parking-place/ParkingPlaceEventHandler/ParkingPlaceEventHandler/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using ParkingPlaceEventHandler;
+using SenseNet.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ParkingPlaceService>();
+builder.Services.Configure<ParkingPlaceOptions>(options => 
+    builder.Configuration.Bind("ParkingPlace", options));
+
+builder.Services.AddSenseNetClientWithUserRepositories(options =>
+    {
+        builder.Configuration.Bind("sensenet:repository", options);
+    },
+    contentTypes =>
+    {
+        contentTypes.Add<ParkingPlaceBooking>();
+    })
+    .AddSenseNetEmailSender(options => builder.Configuration.Bind("sensenet:Email", options));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("ParkingPlaceService", async (
+    [FromBody] ParkingPlaceChanged requestBody, ParkingPlaceService service, HttpContext httpContext) =>
+{
+    await service.HandlePostAsync(requestBody, httpContext);
+});
+
+app.Run();
