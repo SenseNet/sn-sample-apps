3b5963ba542c335013b0ad164410809764f50789
vargajoe <varga.yosh@gmail.com>
2023-10-18 10:28:52 +0200

    visitor welcome message

--- src/parking-place/react-app/src/components/ResvervationContent/Reservations.tsx
+++ src/parking-place/react-app/src/components/ResvervationContent/Reservations.tsx
@@ -6,10 +6,12 @@ import ParkingSlots from "./ParkingSlots";
 import Confirm from "./Confirm";
 import { useState } from "react";
 import dayjs from "dayjs";
+import { useOidcAuthentication } from "@sensenet/authentication-oidc-react";
 
 function Reservations() {
   const styles = UseStyles(ReservationStyles);
-  const [selectedAction, setSelectedAction] = useState("reserve");
+  const { oidcUser } = useOidcAuthentication();
+  const [selectedAction, setSelectedAction] = useState("reserve");  
   const [selectedSlot, setSelectedSlot] = useState(0);
   const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs>(
     dayjs(new Date())
@@ -18,16 +20,25 @@ function Reservations() {
   return (
     <Box sx={styles.content} className="content">
       <h1>Parking Slots</h1>
-      <div className="reservation-container">
-        <DatePicker
-          selectedDate={selectedDate}
-          setSelectedDate={setSelectedDate}
-        />
-        <ParkingSlots selectedSlot={selectedSlot} setSelectedSlot={setSelectedSlot} selectedDate={selectedDate} setSelectedAction={setSelectedAction} />
-      </div>
-      <Confirm selectedSlot={selectedSlot} resetSelectedSlot={setSelectedSlot} selectedDate={selectedDate} selectedAction={selectedAction} />;
+      {(oidcUser && (
+        <>
+          <div className="reservation-container">
+            <DatePicker
+              selectedDate={selectedDate}
+              setSelectedDate={setSelectedDate}
+            />
+            <ParkingSlots selectedSlot={selectedSlot} setSelectedSlot={setSelectedSlot} selectedDate={selectedDate} setSelectedAction={setSelectedAction} />
+          </div>
+          <Confirm selectedSlot={selectedSlot} resetSelectedSlot={setSelectedSlot} selectedDate={selectedDate} selectedAction={selectedAction} />
+        </>
+      )) || (
+        <div className="welcome-message">
+          <div>Sign in to reserve your parking spot effortlessly.</div>
+          <div>Enjoy the convenience of secure parking with just a click!</div>
+        </div>
+      )}
     </Box>
-  );
+  )
 }
 
 export default Reservations;
